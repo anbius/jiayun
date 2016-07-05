f287e7a7d50e59f16e130ba94b6413a1df60d7e2
anbius <772708673@qq.com>
2016-07-05 19:03:52 +0800

    Initial commit

--- Application/Api/Controller/VeterinaryController.class.php
+++ Application/Api/Controller/VeterinaryController.class.php
@@ -10,7 +10,7 @@ class VeterinaryController extends ApiController {
     }
 	
 	/*
-	 * 获取兽医信息  今天是2016年7月5号  萧亚轩
+	 * 获取兽医信息  今天是2016年7月5号  萧亚轩 最深
 	 */
 	public function getList(){
 		$map['status'] = array('eq',1);
