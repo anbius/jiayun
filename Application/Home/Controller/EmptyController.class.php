333577c8401e361b878ceffe7592e5db1aad7851
anbius <772708673@qq.com>
2016-10-27 19:39:04 +0800

    dknfdsljnd

--- Application/Home/Controller/EmptyController.class.php
+++ Application/Home/Controller/EmptyController.class.php
@@ -10,7 +10,7 @@
 namespace Home\Controller;
 
 /**
- * 空模块，主要用于显示404页面，请不要删除
+ *我打电话isi大房东  空模块，主要用于显示404页面，请不要删除
  */
 class EmptyController extends HomeController{
 	//没有任何方法，直接执行HomeController的_empty方法
